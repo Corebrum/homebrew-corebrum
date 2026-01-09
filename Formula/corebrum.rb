48b0faa104eee28ee964472f4b40c923847d2b81
Chris Matthieu <chris@matthieu.us>
2026-01-09 13:47:16 -0700

    Update corebrum.rb

--- Formula/corebrum.rb
+++ Formula/corebrum.rb
@@ -1,19 +1,24 @@
-# This is a template Homebrew formula for Corebrum
-# Copy this to your homebrew-corebrum tap repository at Formula/corebrum.rb
-# The GitHub Actions workflow will automatically update the version and checksums
-
 class Corebrum < Formula
-    desc "Unified Corebrum platform - daemon, CLI, and mesh operating system for decentralized computing"
-    homepage "https://github.com/corebrum/corebrum"
-    license "All Rights Reserved"
+  desc "Unified Corebrum platform - daemon, CLI, and mesh operating system for decentralized computing"
+  homepage "https://github.com/corebrum/corebrum"
+  license "All Rights Reserved"
   version "0.2.132"
-  
-                                on_macos do
-                                        if Hardware::CPU.intel?
-                                                url "https://corebrum-releases.s3.amazonaws.com/releases/v0.2.132/corebrum-x86_64-apple-darwin.tar.xz"
-                                                sha256 "ed71be5177e6d519dc64a4a42d1e167a01a879ec735e78fca5b9274fbe63ddd9"
-                                        else
-                                                url "https://corebrum-releases.s3.amazonaws.com/releases/v0.2.132/corebrum-aarch64-apple-darwin.tar.xz"
-                                                sha256 "c84664f87362cf180fe362aef27b46745b6c6c7f75ac180bc721a04d9b0fa743"
-                                        end
-                                end
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://corebrum-releases.s3.amazonaws.com/releases/v0.2.132/corebrum-x86_64-apple-darwin.tar.xz"
+      sha256 "..."
+    else
+      url "https://corebrum-releases.s3.amazonaws.com/releases/v0.2.132/corebrum-aarch64-apple-darwin.tar.xz"
+      sha256 "..."
+    end
+  end
+
+  def install
+    bin.install "corebrum"
+  end
+
+  test do
+    system "#{bin}/corebrum", "--version"
+  end
+end
